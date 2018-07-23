e3387e5f8b5d7aecc320401f4914cba2144677a8
Adam Chlebek <CHLEBEAD@mail.gvsu.edu>
2018-07-23 16:52:56 -0400

    Add project files.

---
+++ Fortnite API/Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.IO
+Imports System.Net
+Imports Newtonsoft
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim web As WebRequest = WebRequest.Create("https://api.fortnitetracker.com/v1/profile/pc/smokeyfishjr")
+        Dim reader As StreamReader
+        Dim webResponse As WebResponse
+        Dim fullJson As String
+
+        web.Headers.Add("TRN-Api-Key", "bea985f1-1bfb-46b0-acd6-88ea399ebfcb")
+
+        webResponse = web.GetResponse()
+        reader = New StreamReader(webResponse.GetResponseStream())
+
+        fullJson = reader.ReadToEnd()
+
+        Dim smokeyfish As User = New User()
+
+        Newtonsoft.Json.JsonConvert.PopulateObject(fullJson, smokeyfish)
+
+        TextBox1.Text = smokeyfish.
+    End Sub
+End Class
---
+++ Fortnite API/User.vb
@@ -0,0 +1,6 @@
+﻿Public Class User
+    Sub New()
+        Dim EpicUserHandle As String
+        Dim platformNameLong As String
+    End Sub
+End Class
